2f14e7664eaa443a027cf0fe0056cb5a3b50afe0
Stephanie Cheney <steph@scheneyphotography.com>
2020-03-23 21:40:26 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -41,7 +41,7 @@ def total_gross(source)
     
   element_index=0
    while element_index<source[director_index][:movies].size do
-   total += directors_totals(source)
+   total += directors_totals(source)[:name]
    element_index+=1 
   end
   director_index +=1 
